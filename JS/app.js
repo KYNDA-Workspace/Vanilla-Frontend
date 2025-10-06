76f8f9eca6a87480e243879a927813c54b9d6c32
Drewzlla <ohitzandrew@gmail.com>
2025-10-06 19:12:00 +0100

    did this before but didn't commit

--- JS/app.js
+++ JS/app.js
@@ -37,26 +37,4 @@ getStartedBtn.addEventListener("click", () => {
     }
 });
 
-console.log("✅ app.js loaded successfully");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const signupBtn = document.querySelector(".signup-btn");
-  const loginBtn = document.querySelector(".login-btn");
-
-  console.log("signupBtn:", signupBtn);
-  console.log("loginBtn:", loginBtn);
-
-  if (signupBtn) {
-    signupBtn.addEventListener("click", () => {
-      console.log("Sign Up clicked");
-      window.location.href = "user-selection.html";
-    });
-  }
-
-  if (loginBtn) {
-    loginBtn.addEventListener("click", () => {
-      console.log("Login clicked");
-      window.location.href = "Pages/login/login.html";
-    });
-  }
-});
